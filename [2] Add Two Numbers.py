8dcfb37a6ad8675d910c708c46e75c4efbfaf3e9
Vishwa Prakash <121592353+VishTheFish42@users.noreply.github.com>
2026-04-07 00:02:00 -0700

    Add problem statement for 'Add Two Numbers'
    Added a problem statement for adding two numbers represented by linked lists.

---
+++ [2] Add Two Numbers.py
@@ -0,0 +1,28 @@
+'''
+PROBLEM 2
+Add Two Numbers
+
+
+FOCUS
+Linked List, Math, Recursion
+
+
+DESCRIPTION
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each 
+of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+
+CONSTRAINTS
+[1] The number of nodes in each linked list is in the range [1, 100].
+[2] 0 <= Node.val <= 9
+[3] It is guaranteed that the list represents a number that does not have leading zeros.
+'''
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+

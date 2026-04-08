5d97a19b462dd7574a5a5814aebf38566c1e114f
Vishwa Prakash <121592353+VishTheFish42@users.noreply.github.com>
2026-04-08 00:05:47 -0700

    Update [1] Two Sum.py

--- [1] Two Sum.py
+++ [1] Two Sum.py
@@ -27,6 +27,8 @@ def twoSum(nums, target):
 
     for index, num in enumerate(nums):
         num2 = target - num
+        
         if num2 in nums_hash:
             return [index, nums_hash[num2]]
+        
         nums_hash[num] = index

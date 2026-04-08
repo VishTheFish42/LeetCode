e23c033d3ffe2aa7f7060126c6a685f4a400ae57
Vishwa Prakash <121592353+VishTheFish42@users.noreply.github.com>
2026-04-08 00:50:22 -0700

    Add problem statement for Median of Two Sorted Arrays
    Added problem description and constraints for the median of two sorted arrays.

---
+++ [4] Median of Two Sorted Arrays.py
@@ -0,0 +1,24 @@
+'''
+PROBLEM 4
+Median of Two Sorted Arrays
+
+
+FOCUS
+Array, Binary Search, Divide and Conquer
+
+ 
+DESCRIPTION
+Given two sorted arrays "nums1" and "nums2" of size "m" and "n" respectively, return the median of the two sorted arrays.
+
+The overall run time complexity should be O(log (m+n)).
+
+
+CONSTRAINTS
+[1] nums1.length == m
+[2] nums2.length == n
+[3] 0 <= m <= 1000
+[4] 0 <= n <= 1000
+[5] 1 <= m + n <= 2000
+[6] -10^6 <= nums1[i], nums2[i] <= 10^6
+'''
+

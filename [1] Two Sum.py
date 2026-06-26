ac270e43c01a34f3bfbdbe1b1a0336e8e8387507
Vishwa Prakash <121592353+VishTheFish42@users.noreply.github.com>
2026-06-26 11:51:51 -0700

    Add twoSum function for solving Two Sum problem
    Implemented twoSum function to find indices of two numbers that add up to target.

--- [1] Two Sum.py
+++ [1] Two Sum.py
@@ -2,18 +2,13 @@
 PROBLEM 1
 Two Sum
 
-
 FOCUS
 Array, Hash Table
 
-
 DESCRIPTION
 Given an array of integers "nums" and an integer "target", return indices of the two numbers such that they add up to "target".
-
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
 You can return the answer in any order.
-
 
 CONSTRAINTS
 [1] 2 <= nums.length <= 10^4
@@ -22,6 +17,7 @@ CONSTRAINTS
 [4] Only one valid answer exists.
 '''
 
+# SOLUTION
 def twoSum(nums, target):
     nums_hash = {}
 
@@ -32,3 +28,12 @@ def twoSum(nums, target):
             return [index, nums_hash[num2]]
         
         nums_hash[num] = index
+
+'''
+FOLLOW-UP
+Can you come up with an algorithm that is less than O(n^2) time complexity?
+
+ANALYSIS
+Time Complexity: O(n)
+Space Complexity: O(n)
+'''

6732e2a87895bfc08eb5d6a1c169354289cca2ee
Vishwa Prakash <121592353+VishTheFish42@users.noreply.github.com>
2026-06-28 13:27:40 -0700

    Implement median finding for two sorted arrays
    Added a solution for finding the median of two sorted arrays using binary search.

--- [4] Median of Two Sorted Arrays.py
+++ [4] Median of Two Sorted Arrays.py
@@ -1,17 +1,13 @@
 '''
-PROBLEM 4
+PROBLEM 4 (HARD)
 Median of Two Sorted Arrays
-
 
 FOCUS
 Array, Binary Search, Divide and Conquer
-
  
 DESCRIPTION
 Given two sorted arrays "nums1" and "nums2" of size "m" and "n" respectively, return the median of the two sorted arrays.
-
 The overall run time complexity should be O(log (m+n)).
-
 
 CONSTRAINTS
 [1] nums1.length == m
@@ -22,3 +18,48 @@ CONSTRAINTS
 [6] -10^6 <= nums1[i], nums2[i] <= 10^6
 '''
 
+# SOLUTION
+def findMedianSortedArrays(nums1, nums2):
+    if (len(nums1) > len(nums2)):
+        temp = nums2
+        nums2 = nums1
+        nums1 = temp
+
+    m = len(nums1)
+    n = len(nums2)
+
+    left = 0
+    right = len(nums1)
+    i = (left + right) / 2
+
+    j = ((m + n + 1) / 2) - i
+
+    max_left1 = nums1[i - 1]  if i > 0 else float('-inf')
+    min_right1 = nums1[i]     if i < m else float('inf')
+    max_left2  = nums2[j - 1] if j > 0 else float('-inf')
+    min_right2 = nums2[j]     if j < n else float('inf')
+
+    while ((max_left1 > min_right2) or (max_left2 > min_right1)):
+        if (max_left1 > min_right2):
+            right = i - 1
+        else:
+            left = i + 1
+
+        i = (left + right) // 2
+        j = ((m + n + 1) // 2) - i
+
+        max_left1 = nums1[i - 1]  if i > 0 else float('-inf')
+        min_right1 = nums1[i]     if i < m else float('inf')
+        max_left2 = nums2[j - 1]  if j > 0 else float('-inf')
+        min_right2 = nums2[j]     if j < n else float('inf')
+
+    if ((m + n) % 2 == 1):
+        return max(max_left1, max_left2)
+    else:
+        return (max(max_left1, max_left2) + min(min_right1, min_right2)) / 2.0
+
+'''
+ANALYSIS
+Time Complexity: O(log(min(m, n)))
+Space Complexity: O(1)
+'''

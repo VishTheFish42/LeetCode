e1ff377796b82231c9f5d02fbb3340123056393d
Vishwa Prakash <121592353+VishTheFish42@users.noreply.github.com>
2026-07-04 14:28:44 -0700

    Enhance comments with explanation and analysis
    Added detailed explanation and analysis for the algorithm.

--- [3] Longest Substring Without Repeating Characters.py
+++ [3] Longest Substring Without Repeating Characters.py
@@ -44,8 +44,18 @@ def lengthOfLongestSubstring(s):
   
    return max_length
 
-
 '''
+EXPLANATION
+[1] We simply return the length of s if it is 0 or 1 (since that substring must be the longest with no duplicate characters).
+[2] Otherwise, we initialize two pointers start and end, max_length as 1, and a hash table with the first letter mapped to index 0 
+    (this hash table tracks the last index we see a certain character).
+[3] We iterate across each character in the string starting from the second. If the letter is not in the hash table, end is set to the current index.
+[4] Otherwise, we have seen the letter before. If start is greater than one more than the last seen index of that letter, end is set to the current index. 
+    Otherwise, start is set to the the last index of the letter + 1.
+[5] If the current length of the substring (given by start and end) is greater than max_length, we set max_length to the new length.
+[6] We set the last seen index of the current letter to the current index.
+[7] Outside of the loop, we return max_length.
+
 ANALYSIS
 Time Complexity: O(n)
 Space Complexity: O(n)
